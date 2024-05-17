f9e0d0a6a155aae2b73e24f249fc2cb8a306d35f
Sergio Benavides <sergiobenav@proton.me>
2024-05-16 21:42:51 -0600

    setup app for deploy

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,16 @@
 import { Hono } from 'hono'
 import { logger } from "hono/logger"
 import { expenseRoute } from "./routes/expenses.ts";
+import { serveStatic } from 'hono/bun'
 
 const app = new Hono()
 
 app.use("*", logger())
-app.get('/', (c) => c.text('Hono!'))
+
+app.use('*', serveStatic({ root: './frontend/dist' }))
+app.get('*', serveStatic({ path: './frontend/dist/index.html' }))
 
 app.route("/api/expenses", expenseRoute)
 
 export default app
+
